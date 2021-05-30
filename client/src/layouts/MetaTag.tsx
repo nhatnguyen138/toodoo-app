b7a934ec1760c6866b079bf6396c1e3a6c5e35d3
nhatnguyen9813 <nguyenkimnhat130898@gmail.com>
2021-05-30 19:03:29 +1000

    make adjustment to MetaTag file

--- client/src/layouts/MetaTag.tsx
+++ client/src/layouts/MetaTag.tsx
@@ -1,13 +1,15 @@
 import { Helmet } from 'react-helmet'
+import { MetaTagDef } from '../utils/TypeDef'
+import Logo from '../assets/TooDooLogo.png'
 
-export default function MetaTag(props:any) {
+export default function MetaTag(props:MetaTagDef) {
     let {section} = props
     if (section!=="")
         section+=" | "
     return (
         <Helmet>
             <title>{section}TooDoo</title>
-            <link rel="icon" href="TooDooLogo.png"/>
+            <link rel="icon" href={Logo}/>
         </Helmet>
     )
 }
